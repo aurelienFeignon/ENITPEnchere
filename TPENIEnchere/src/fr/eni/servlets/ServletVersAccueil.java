fa80c17b6aec8662fe49dfd6d853a553fd202b56
jerem <jerem@LAPTOP>
2021-01-07 18:03:07 +0100

    Modif de la servlet accueil qui prend en charge l'affichage d'articles.

--- TPENIEnchere/src/fr/eni/servlets/ServletVersAccueil.java
+++ TPENIEnchere/src/fr/eni/servlets/ServletVersAccueil.java
@@ -1,11 +1,17 @@
 package fr.eni.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.bll.articleManageur;
+import fr.eni.bo.Article;
+import fr.eni.utils.BusinessException;
 
 /**
  * Servlet implementation class ServletVersAccueil
@@ -18,16 +24,34 @@ public class ServletVersAccueil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.sendRedirect("PageAccueil");
+		//Afficher les articles de la base de données
+		articleManageur articleManager  = new articleManageur();
+		List<Article> listeArticle =null;
+		try {
+			listeArticle = articleManager.selectAll();
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("listeArticle", listeArticle);
+		this.getServletContext().getRequestDispatcher("/PageAccueil").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		
+		//On recup les données
+		String recherche = request.getParameter("recherche"); //Il faut un select where %recherche%
+		String categorie = request.getParameter("categorie");
+		String menu = request.getParameter("menu");
+		if(request.getParameter("encheresOuvertes") != null){
+			
+		}
+		if(request.getParameter("encheresEnCours") != null){
+			
+		}
+		this.getServletContext().getRequestDispatcher("/PageAccueil").forward(request, response);
 	}
-
+	
 }
