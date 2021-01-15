6488e25690b222b3b592297082a052a0df4b10e0
aurel <aurel@LAPTOP-NLVKED1M>
2021-01-15 18:14:17 +0100

    javadoc utilisateurManageur

--- TPENIEnchere/src/fr/eni/bll/UtilisateurManageur.java
+++ TPENIEnchere/src/fr/eni/bll/UtilisateurManageur.java
@@ -9,17 +9,34 @@ import fr.eni.dal.DAOFactory;
 import fr.eni.dal.UtilisateurDao;
 import fr.eni.utils.BusinessException;
 
+/**
+ * Classe en charge d'acceder au methode
+ * 
+ * @author aurel
+ * @version TPENIEnchere - v1.0
+ * @date 15 janv. 2021 - 15:18:41
+ */
 public class UtilisateurManageur {
 	private UtilisateurDao utilisateurDao;
 
 	/**
 	 * @param utilisateurDao
 	 */
+	/**
+	 * Constructeur permetant d'instancier utilisateurDao grace au Dao factory
+	 */
 	public UtilisateurManageur() {
 
 		this.utilisateurDao = DAOFactory.getUtilisateur();
 	}
 
+	/**
+	 * Méthode en charge de déléguer l'insertion d'un utilisateur à la dal après des
+	 * verification
+	 * 
+	 * @param utilisateur utilisateur à inserer
+	 * @throws BusinessException
+	 */
 	public void insert(Utilisateur utilisateur) throws BusinessException {
 		BusinessException businessException = new BusinessException();
 
@@ -34,12 +51,27 @@ public class UtilisateurManageur {
 		}
 	}
 
+	/**
+	 * Méthode en charge de selectionner un utilisateur par son id
+	 * 
+	 * @param id de l'utilisateur
+	 * @return l'utilisateur si il existe
+	 * @throws BusinessException
+	 */
 	public Utilisateur selectId(int id) throws BusinessException {
 		Utilisateur utilisateur = null;
 		utilisateur = this.utilisateurDao.selectId(id);
 		return utilisateur;
 	}
 
+	/**
+	 * Méthode en charge de verifier si le pseudo et l'email corresponde à un mdp
+	 * 
+	 * @param pseudo email ou pseudo
+	 * @param mdp    mot de passe
+	 * @return un utilisateur
+	 * @throws BusinessException
+	 */
 	public Utilisateur VerifConnexion(String pseudo, String mdp) throws BusinessException {
 		Utilisateur utilisateur = null;
 		BusinessException businessException = new BusinessException();
@@ -62,12 +94,28 @@ public class UtilisateurManageur {
 		}
 	}
 
+	/**
+	 * Méthode en charge de selectionner un utilisateur en fonction de son email ou
+	 * de son pseudo
+	 * 
+	 * @param pseudo email ou pseudo
+	 * @return un utilisateur si il est existant
+	 * @throws BusinessException
+	 */
 	public Utilisateur selectPseudo(String pseudo) throws BusinessException {
 		Utilisateur utilisateur = null;
 		utilisateur = this.utilisateurDao.selectPseudo(pseudo);
 		return utilisateur;
 	}
 
+	/**
+	 * Méthode en charge d'update un utilisateur
+	 * 
+	 * @param utilisateur utilisateur à update
+	 * @param id          id de l'utilisateur à update
+	 * @throws BusinessException
+	 * @throws SQLException
+	 */
 	public void update(Utilisateur utilisateur, int id) throws BusinessException, SQLException {
 		BusinessException businessException = new BusinessException();
 
@@ -82,6 +130,14 @@ public class UtilisateurManageur {
 		}
 	}
 
+	/**
+	 * Méthode en charge d'enlever des credits a un utilisateur
+	 * 
+	 * @param utilisateur utilisateur à update
+	 * @param debit       montant a lui retirer
+	 * @throws BusinessException
+	 * @throws SQLException
+	 */
 	public void enleveCredit(Utilisateur utilisateur, int debit) throws BusinessException, SQLException {
 		BusinessException businessException = new BusinessException();
 		double credit = utilisateur.getCredit() - debit;
@@ -98,6 +154,14 @@ public class UtilisateurManageur {
 
 	}
 
+	/**
+	 * Méthode en charge d'ajouter des credits à un utilisateur
+	 * 
+	 * @param utilisateur utilisateur à update
+	 * @param debit       credit à ajouter
+	 * @throws BusinessException
+	 * @throws SQLException
+	 */
 	public void AjouterCredit(Utilisateur utilisateur, int debit) throws BusinessException, SQLException {
 		BusinessException businessException = new BusinessException();
 		double credit = utilisateur.getCredit() + debit;
@@ -114,17 +178,39 @@ public class UtilisateurManageur {
 
 	}
 
+	/**
+	 * Méthode en charge de selectionner tout les utilisateurs présent en bd
+	 * 
+	 * @return la liste d'utilisateur
+	 * @throws BusinessException
+	 */
 	public List<Utilisateur> selectAll() throws BusinessException {
 		List<Utilisateur> utilisateurs = this.utilisateurDao.selectAll();
 		return utilisateurs;
 
 	}
 
+	/**
+	 * Méthode en charge de supprimer un utilisateur
+	 * 
+	 * @param id de l'utilisateur à supprimer
+	 * @throws BusinessException
+	 */
 	public void delete(int id) throws BusinessException {
 
 		this.utilisateurDao.delete(id);
 	}
 
+	/**
+	 * Méthode en charge de valider le pseudo, il verifie aussi que le pseudo n'est
+	 * pas existant en bdd, de plus la methode verifie que le pseudo est en
+	 * caractere alphanumerique, qu'il ne fait pas plus de 30 lettres et qu'il n'est
+	 * pas null
+	 * 
+	 * @param utilisateur       utilisateur à verifier
+	 * @param businessException
+	 * @throws BusinessException
+	 */
 	private void validerPseudo(Utilisateur utilisateur, BusinessException businessException) throws BusinessException {
 		// Vérification de l'existence de Pseudo
 		List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
@@ -149,18 +235,42 @@ public class UtilisateurManageur {
 		}
 	}
 
+	/**
+	 * Méthode en charge de valider le nom de l'utilisateur qu'il ne soit pas null
+	 * et que ne fait pas plus de 30 lettres
+	 * 
+	 * @param utilisateur
+	 * @param businessException
+	 */
 	private void validerNom(Utilisateur utilisateur, BusinessException businessException) {
 		if (utilisateur.getNom() == null || utilisateur.getNom().length() > 30) {
 			businessException.ajouterErreur(CodeResultatBll.NOM_INVALIDE);
 		}
 	}
 
+	/**
+	 * Méthode en charge de valider le prenom de l'utilisateurqu'il ne soit pas null
+	 * et que ne fait pas plus de 30 lettres
+	 * 
+	 * @param utilisateur
+	 * @param businessException
+	 */
 	private void validerPrenom(Utilisateur utilisateur, BusinessException businessException) {
 		if (utilisateur.getPrenom() == null || utilisateur.getPrenom().length() > 30) {
 			businessException.ajouterErreur(CodeResultatBll.PRENOM_INVALIDE);
 		}
 	}
 
+	/**
+	 * Méthode en charge de valider l'email, il verifie aussi que le pseudo n'est
+	 * pas existant en bdd, de plus la methode verifie que le pseudo est en
+	 * caractere alphanumerique, qu'il ne fait pas plus de 30 lettres et qu'il n'est
+	 * pas null
+	 * 
+	 * @param utilisateur
+	 * @param businessException
+	 * @throws BusinessException
+	 */
 	private void validerEmail(Utilisateur utilisateur, BusinessException businessException) throws BusinessException {
 		// Vérification de l'existence de l'email
 		List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
